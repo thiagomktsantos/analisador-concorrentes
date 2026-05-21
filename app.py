6662595955dff46dcd0b2f538b16f37d573fe3fe
thiagomktsantos <thiagomktsantos@gmail.com>
2026-05-21 17:00:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -578,7 +578,7 @@ div[data-testid="stTabs"] button[role="tab"] {
     font-family: 'DM Sans', sans-serif !important;
     padding: 10px 32px !important;
     color: #9ca3af !important;
-    border-bottom: 2px solid transparent !important;
+    border-radius: 8px 8px 0px 0px !important;
     margin-bottom: -2px !important;
     text-transform: uppercase;
 }
