600e88ed3973e3df19c5890e6fea661044cf3473
thiagomktsantos <thiagomktsantos@gmail.com>
2026-05-25 12:31:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4102,7 +4102,6 @@ setTimeout(syncHeight, 200); setTimeout(syncHeight, 600);
                         f'<path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>'
                         f'</svg></button>'
                 )
-        lapiz_abas_html += f'<button class="lapiz-btn" onclick="triggerLapiz(\'{e[\'nome']}\')" title="Editar {e[\'nome\']}"><svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg></button>'
 
         components.html(f"""
 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
