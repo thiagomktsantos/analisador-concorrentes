52019758ac8d3f2a5b719ae0407e0b5eaedd98a9
thiagomktsantos <thiagomktsantos@gmail.com>
2026-05-27 12:15:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8176,7 +8176,7 @@ function syncHeight() {{
         try {{
             if (frames[i].contentWindow === window) {{
                 frames[i].style.height = (h + 12) + 'px';
-                iframes[i].style.marginTop = '-101px';
+                frames[i].style.marginTop = '-101px';
                 break;
             }}
         }} catch(e) {{}}
